40e75aa0e5bbda94d5f854fc58aae09be3fe7677
Ratchanon Panmas <qq.panmas@gmail.com>
2022-12-09 11:19:59 +0700

    💄 Fix size

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,3 +1,15 @@
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div className="flex flex-col p-5">
+      <h1 className="text-7xl text-center">จะกินไฬดี</h1>
+      <h2>LOGIN</h2>
+      <input
+        className="px-10 py-5 border-2 rounded-full text-center text-xl"
+        type="email"
+        name=""
+        id=""
+        placeholder="Email Address"
+      />
+    </div>
+  );
 }
