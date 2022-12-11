a272a310dab4bbd16a8096a70d577b8f2e8eca04
WannakornU <jam0924914626@gmail.com>
2022-12-11 12:48:07 +0700

    welcome page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,12 @@ import Image from "next/image";
 
 export default function WelcomePage() {
   return (
-    <div className="relative">
-      <Image
-        src={"/welcomeimage.png"}
-        alt="welcome"
-        width={1163}
-        height={872}
-      />
-      <h1 className="absolute">จะกินไฬดี</h1>
+    <div className="flex items-end h-[768px] bg-no-repeat bg-cover bg-[url('/welcomeimage.png')] ">
+      <div className="bg-gradient-to-t from-black via-black w-full h-fit text-white font-Kanit text-center flex flex-col items-center  pb-10 px-5">
+        <h1 className="text-7xl w-fit font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary to-white">จะกินไฬดี</h1>
+        <p className="mb-10 font-light mt-2">แอพที่จะช่วยคุณได้เห็นรีวิวโรงอาหารในรั้วจุฬาฯและ <br /> ยังสามารถคุณสามารถรีวิวร้านอาหารได้เองอีกด้วย</p>
+        <a href="" className="bg-primary px-20 py-2 rounded-full text-2xl uppercase">Get Start</a>
+      </div>
     </div>
   );
 }
