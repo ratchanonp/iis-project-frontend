052e35133b9cf76b81c8618e57c5e7a9cdd8e7df
Ratchanon Panmas <qq.panmas@gmail.com>
2022-12-11 21:23:56 +0700

    mobile mock

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,7 @@
 import { Head, Html, Main, NextScript } from "next/document";
-import { useRouter } from "next/router";
 import QRCode from "react-qr-code";
 
 export default function Document() {
-
-	const router = useRouter();
-	
 	return (
 		<Html>
 			<Head />
@@ -15,10 +11,13 @@ export default function Document() {
 					<NextScript />
 				</div>
 				<div className="flex flex-col font-Kanit">
-					<h1 className="text-9xl font-semibold text-primary">จะกินไฬดี</h1>
-					<h2 className="text-5xl font-medium">Web Application</h2>
+					<h1 className="text-7xl font-semibold text-primary">จะกินไ<span className="text-9xl font-bold">ฬ</span>ดี</h1>
+					<h2 className="text-5xl font-medium underline decoration-primary">Web Application</h2>
+
+					<p className="mt-10 text-2xl leading-10">มาเรียนที่<span className="underline decoration-primary decoration-wavy mx-2">จุฬา</span>แล้วไม่รู้<span className="underline decoration-primary decoration-wavy mx-2">จะกินไรดี</span></p>
+					<p className="text-2xl"><span className="text-primary">จะกินไฬดี</span> ช่วยได้</p>
 					
-					<QRCode value={""} size={150} className="mt-20"/>
+					<QRCode value={""} size={200} className="mt-20"/>
 				</div>
 			</body>
 		</Html>
